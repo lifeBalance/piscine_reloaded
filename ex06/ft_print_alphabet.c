103a6f992f437ecbe2e516662bb82e7cbf8f2c60
dummylanguages <dummylanguages@gmail.com>
2021-10-27 12:17:02 +0300

    fix ex06/ft_print_alphabet.c

--- ex06/ft_print_alphabet.c
+++ ex06/ft_print_alphabet.c
@@ -6,18 +6,17 @@
 /*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 00:38:52 by rodrodri          #+#    #+#             */
-/*   Updated: 2021/10/27 11:46:16 by rodrodri         ###   ########.fr       */
+/*   Updated: 2021/10/27 12:13:52 by rodrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_putchar(char ch);
 
-int	main(void)
+void	ft_print_alphabet(void)
 {
 	char	ch;
 
 	ch = 'a';
 	while (ch <= 'z')
 		ft_putchar(ch++);
-	return (0);
 }
