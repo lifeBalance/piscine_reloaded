a82de23413c986fa3b558140e895b48ccaec79a0
dummylanguages <dummylanguages@gmail.com>
2021-10-27 15:50:09 +0300

    add ex13/ft_recursive_factorial.c

---
+++ ex13/ft_recursive_factorial.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rodrodri <rodrodri@student.hive.fi >       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/27 15:33:08 by rodrodri          #+#    #+#             */
+/*   Updated: 2021/10/27 15:49:20 by rodrodri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb < 0)
+		return (0);
+	else if (nb == 0 || nb == 1)
+		return (1);
+	else
+		return (nb * ft_recursive_factorial(nb - 1));
+}
